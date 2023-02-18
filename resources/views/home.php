830c15e5cf9b16ba534041f15f223bc04e23ccf4
Greg <gregory.tuck@hotmail.com>
2023-02-18 18:39:30 +0000

    Create basic views

---
+++ resources/views/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Currency Conversion</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                width: 100%;
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body style="width: 100%; display: flex; flex-direction: column; align-items: center;">
+        <a href="/users/create">Create new user</a>
+    </body>
+</html>
